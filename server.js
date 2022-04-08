7770ea421cd1bf26bbcd7c31b885e4cf190bb0e2
MONICA-K-9600 <monicamonisa2@gmail.com>
2022-04-08 20:22:26 +0530

    commit

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static('./dist/angular-app-heroku'));
+app.use(express.static('./dist/talent-center'));
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/angular-app-heroku/' }),
+    res.sendFile('index.html', { root: 'dist/talent-center/' }),
 );
 app.listen(process.env.PORT || 8080);
