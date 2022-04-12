06e4ee931db500545a60348a8cfe974ba99efc98
MONICA-K-9600 <monicamonisa2@gmail.com>
2022-04-12 13:45:09 +0530

    commiting changes

--- src/app/main-view/main-view.component.ts
+++ src/app/main-view/main-view.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Board } from 'src/app/models/board.model';
 import { Column } from 'src/app/models/column.model';
 import{ CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
-
+import { ActivationEnd } from '@angular/router';
+import { __values } from 'tslib';
 @Component({
   selector: 'app-main-view',
   templateUrl: './main-view.component.html',
@@ -10,33 +11,55 @@ import{ CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag
 })
 
 export class MainViewComponent implements OnInit {
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
+  title = 'TalentCenter';
+  term!: string;
+  filterData=[{firstName:
+    "Kartikeya Sita"},
+    {firstName:"Jagadisha Gopinatha"},
+    {firstName: "Saraswati Brahma"},
+    {firstName:"Lakshmana Madhav"},
+     {firstName: "Shanta Indrani"},
+     {firstName: "Gargi Vikrama"},
+     {firstName: "Rameshvara Pankaja"},
+     {firstName: "Girisha Baladeva"},
+     {firstName: "Mohana Vijaya"},
+     {firstName: "Mani Padmavati"},
+     {firstName: "Indrani Gauri"}, 
+     {firstName: "Shiva Sita"},
+     {firstName: "Sunita Drupam"},
+     {firstName: "Harishmitha V"},
+     {firstName: "Bala Murali"},
+     {firstName: "Kalyani Madhavi"},
+     {firstName: "Jayanti Kapila"},
+     {firstName: "Maya Kanti"},
+     {firstName: "Bhima Kapila"},
+     {firstName: "Karthikeyan Reva"}
+  ]
+   ngOnInit(): void {
+      throw new Error('Method not implemented.');
   }
-searchInput:any;
-datas:any;
- 
-  board: Board = new Board('Open', [
-    new Column('open', [
+board: Board = new Board('Open', [
+    new Column('Open - 2', [
       "Maya Kanti",
       "Bhima Kapila",
       "Karthikeyan Reva"]),
-    new Column('Contacted', [
+    new Column('Contacted - 3 ', [
       "Bala Murali",
       "Kalyani Madhavi",
       "Jayanti Kapila"]),
-    new Column('written test', [
+    new Column('Written test - 5', [
       "Mani Padmavati",
       "Indrani Gauri", 
       "Shiva Sita",
-      "Sunita Drupam"]),
-    new Column('Technical Round', [
+      "Sunita Drupam",
+      "Harishmitha V"]),
+    new Column('Technical round - 5', [
       "Shanta Indrani",
       "Gargi Vikrama",
       "Rameshvara Pankaja",
       "Girisha Baladeva",
       "Mohana Vijaya" ]),
-    new Column('Culture fit Round', [
+    new Column('Culture fit round - 4', [
       "Kartikeya Sita",
       "Jagadisha Gopinatha",
       "Saraswati Brahma",
@@ -52,4 +75,7 @@ datas:any;
         event.currentIndex);
     }
   }
+  active(){
+   this.term;
+  } 
 }
